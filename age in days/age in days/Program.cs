19faa75de272b64766d5a65450563ea59f845367
Jacob Lueke <jacoblueke@gmail.com>
2021-10-13 09:05:20 +0100

    no message

---
+++ age in days/age in days/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace age_in_days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthDay;
+            int today;
+
+            Console.WriteLine("whats your birthday ?");
+            Console.WriteLine("whats todays date ?");
+        }
+    }
+}
